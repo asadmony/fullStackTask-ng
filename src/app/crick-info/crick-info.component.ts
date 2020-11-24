b4c72ae0ff1eb06d792dc8284cb1b82c94afe410
Md. Asaduzzaman <md.asad.mony@gmail.com>
2020-11-25 04:16:02 +0600

    css

--- src/app/crick-info/crick-info.component.ts
+++ src/app/crick-info/crick-info.component.ts
@@ -2,14 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-crick-info',
-  templateUrl: './crick-info.component.html',
+  templateUrl:'./crick-info.component.html',
   styleUrls: ['./crick-info.component.css']
 })
 export class CrickInfoComponent implements OnInit {
-
   constructor() { }
-
   ngOnInit(): void {
+  }
+  live: boolean;
+  result: boolean;
+  button(){
+    this.live = !this.live
+    this.result = !this.result
   }
 
 }
